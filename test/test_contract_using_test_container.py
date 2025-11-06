2f46eda6b7ba985517e3bd2b608016dc406c73e0
Sufiyan <StarKhan6368@gmail.com>
2025-11-06 14:37:30 +0530

    Update dependencies

--- test/test_contract_using_test_container.py
+++ test/test_contract_using_test_container.py
@@ -52,9 +52,10 @@ def stub_container():
         .waiting_for(HttpWaitStrategy(HTTP_STUB_PORT, path="/actuator/health").with_method("GET").for_status_code(200))
     )
     container.start()
-    stream_container_logs(container, name="specmatic-stub")
+    thread = stream_container_logs(container, name="specmatic-stub")
     yield container
     container.stop()
+    thread.join()
 
 
 @pytest.fixture(scope="module")
@@ -71,9 +72,10 @@ def test_container():
         .waiting_for(LogMessageWaitStrategy("Tests run:"))
     )
     container.start()
-    stream_container_logs(container, name="specmatic-test")
+    thread = stream_container_logs(container, name="specmatic-test")
     yield container
     container.stop()
+    thread.join()
 
 
 @pytest.mark.skipif(
